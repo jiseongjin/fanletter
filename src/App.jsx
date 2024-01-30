126c7d6a39bcca9c08e264cf9745b2a6a9a91a85
Your Name <you@your-domain.com>
2024-01-30 17:35:37 +0900

    add:전역스타일링시도

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import GlobalStyle from "GlobalStyle";
 import Router from "shared/Router";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
--- src/components/FanletterMain.jsx
+++ src/components/FanletterMain.jsx
@@ -1,11 +1,9 @@
+import GlobalStyle from "GlobalStyle";
 import React from "react";
 import styled from "styled-components";
 
 const Main = styled.main`
-  display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
 const MemberNames = styled.section`
   height: 50px;
@@ -71,10 +69,6 @@ const RegisterButton = styled.button`
 `;
 
 const FanLetterList = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   width: 600px;
   background-color: black;
   padding: 20px;
@@ -118,59 +112,62 @@ function FanletterMain() {
   //   console.log(date);
 
   return (
-    <Main>
-      <MemberNames>
-        <NameButton>안유진</NameButton>
-        <NameButton>가을</NameButton>
-        <NameButton>레이</NameButton>
-        <NameButton>장원영</NameButton>
-        <NameButton>리즈</NameButton>
-        <NameButton>이서</NameButton>
-      </MemberNames>
-      <FillBox>
-        <FillBoxTexts>
-          <FillContent>닉네임:&nbsp;</FillContent>
-          <NameText
-            type="text"
-            placeholder="최대 20글자까지 작성할 수 있습니다."
-          />
-        </FillBoxTexts>
-        <FillBoxTexts>
-          <FillContent>내용:&nbsp;</FillContent>
-          <ContentText
-            type="text"
-            placeholder="최대 100자까지만 작성할 수 있습니다."
-          />
-        </FillBoxTexts>
-        <article>
-          누구에게 보내실 건가요?&nbsp;
-          <select>
-            <option>안유진</option>
-            <option>가을</option>
-            <option>레이</option>
-            <option>장원영</option>
-            <option>리즈</option>
-            <option>이서</option>
-          </select>
-        </article>
-        <RegisterButton>팬레터 등록</RegisterButton>
-      </FillBox>
-      <FanLetterList>
-        <Fanletter>
-          <Profile
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaW3SfW7ZP7a7QSiL5_hliZmyZukjKufZQwg&usqp=CAU"
-            alt=""
-          />
-          <FanletterContent>
-            <p>sdafsddfasfdsfsgfdgdfgfdgdfgdf</p>
-            <p>dfgdfgfdgdfgdgdfgdfgdf</p>
-            <FanletterDetail>
-              gdfgdfgdfgdfgdfgdfgdfgdfgdfuhdsafjkdshfuhfasduwdhfjfguiehuidfheqiudhqiudhqwiudhqiwudhiqwdhqiwudhqigdfgdfgdf
-            </FanletterDetail>
-          </FanletterContent>
-        </Fanletter>
-      </FanLetterList>
-    </Main>
+    <>
+      <GlobalStyle />
+      <Main>
+        <MemberNames>
+          <NameButton>안유진</NameButton>
+          <NameButton>가을</NameButton>
+          <NameButton>레이</NameButton>
+          <NameButton>장원영</NameButton>
+          <NameButton>리즈</NameButton>
+          <NameButton>이서</NameButton>
+        </MemberNames>
+        <FillBox>
+          <FillBoxTexts>
+            <FillContent>닉네임:&nbsp;</FillContent>
+            <NameText
+              type="text"
+              placeholder="최대 20글자까지 작성할 수 있습니다."
+            />
+          </FillBoxTexts>
+          <FillBoxTexts>
+            <FillContent>내용:&nbsp;</FillContent>
+            <ContentText
+              type="text"
+              placeholder="최대 100자까지만 작성할 수 있습니다."
+            />
+          </FillBoxTexts>
+          <article>
+            누구에게 보내실 건가요?&nbsp;
+            <select>
+              <option>안유진</option>
+              <option>가을</option>
+              <option>레이</option>
+              <option>장원영</option>
+              <option>리즈</option>
+              <option>이서</option>
+            </select>
+          </article>
+          <RegisterButton>팬레터 등록</RegisterButton>
+        </FillBox>
+        <FanLetterList>
+          <Fanletter>
+            <Profile
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaW3SfW7ZP7a7QSiL5_hliZmyZukjKufZQwg&usqp=CAU"
+              alt=""
+            />
+            <FanletterContent>
+              <p>sdafsddfasfdsfsgfdgdfgfdgdfgdf</p>
+              <p>dfgdfgfdgdfgdgdfgdfgdf</p>
+              <FanletterDetail>
+                gdfgdfgdfgdfgdfgdfgdfgdfgdfuhdsafjkdshfuhfasduwdhfjfguiehuidfheqiudhqiudhqwiudhqiwudhiqwdhqiwudhqigdfgdfgdf
+              </FanletterDetail>
+            </FanletterContent>
+          </Fanletter>
+        </FanLetterList>
+      </Main>
+    </>
   );
 }
 
